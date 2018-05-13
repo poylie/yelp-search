330d85bb7f65171cd47115d146594fb11e3c3255
unknown <ephraimfernandezjr@gmail.com>
2018-05-14 00:58:09 +0800

    improve ui for stars and review count

--- handlers/search.go
+++ handlers/search.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"math"
 	"net/http"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/poylie/yelp-search/libhttp"
 )
 
@@ -16,18 +18,22 @@ type Response struct {
 	Total    int        `json:"total"`
 	Keyword  string
 	Location string
+	SortBy   string
 }
 
 // Business struct for each business
 type Business struct {
-	Name        string      `json:"name"`
-	Alias       string      `json:"alias"`
-	Location    Location    `json:"location"`
-	Price       string      `json:"price"`
-	Rating      float64     `json:"rating"`
-	URL         string      `json:"url"`
-	ImageURL    string      `json:"image_url"`
-	Coordinates Coordinates `json:"coordinates"`
+	Name          string   `json:"name"`
+	Alias         string   `json:"alias"`
+	Location      Location `json:"location"`
+	Price         string   `json:"price"`
+	Rating        float64  `json:"rating"`
+	RatingDisplay []bool
+	RatingHalf    bool
+	ReviewCount   int         `json:"review_count"`
+	URL           string      `json:"url"`
+	ImageURL      string      `json:"image_url"`
+	Coordinates   Coordinates `json:"coordinates"`
 }
 
 // Coordinates struct
@@ -49,28 +55,39 @@ type Location struct {
 }
 
 // GetSearch handler
-func GetSearch(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
+func GetSearch(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "text/html")
 
 	tmpl, err := template.ParseFiles("templates/dashboard.html.tmpl", "templates/home.html.tmpl")
 
-	r.ParseForm()
-	// logic part of log in
-	keyword := r.FormValue("keyword")
-	location := r.FormValue("location")
+	request.ParseForm()
+	// get value from search
+	keyword := request.FormValue("keyword")
+	location := request.FormValue("location")
+	sortBy := "best_match"
 
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://api.yelp.com/v3/businesses/search", nil)
+	yelpRequest, _ := http.NewRequest("GET", "https://api.yelp.com/v3/businesses/search", nil)
 
-	q := req.URL.Query()
-	q.Add("term", keyword)
-	q.Add("location", location)
-	req.URL.RawQuery = q.Encode()
+	urlQuery := yelpRequest.URL.Query()
+	if keyword != "" {
+		urlQuery.Add("term", keyword)
+	}
 
-	req.Header.Set("Authorization", "Bearer VNBosgLfAisHadcgnVbBUUawb--mf61EurggtiBUDcdG-StSyzuwQ39yk0n5OGMIbKxcjFlhaHh8c0YNIrbUmv-UXRaHtYNqcEAkrQ8c-5UMXUerZj85DoRZqU3yWnYx")
+	if location != "" {
+		urlQuery.Add("location", location)
+	}
 
-	fmt.Printf("Request: %s\n", req.URL)
-	response, err := client.Do(req)
+	if sortBy != "" {
+		urlQuery.Add("sort_by", sortBy)
+	}
+
+	yelpRequest.URL.RawQuery = urlQuery.Encode()
+
+	yelpRequest.Header.Set("Authorization", "Bearer VNBosgLfAisHadcgnVbBUUawb--mf61EurggtiBUDcdG-StSyzuwQ39yk0n5OGMIbKxcjFlhaHh8c0YNIrbUmv-UXRaHtYNqcEAkrQ8c-5UMXUerZj85DoRZqU3yWnYx")
+
+	logrus.Infof("Request: %s\n", yelpRequest.URL)
+	response, err := client.Do(yelpRequest)
 
 	var responseObject Response
 
@@ -78,18 +95,33 @@ func GetSearch(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
-		//fmt.Printf(string(data))
 
 		json.Unmarshal(data, &responseObject)
 	}
 
 	if err != nil {
-		libhttp.HandleErrorJson(w, err)
+		libhttp.HandleErrorJson(responseWriter, err)
 		return
 	}
 
 	responseObject.Location = location
 	responseObject.Keyword = keyword
 
-	tmpl.Execute(w, responseObject)
+	for elementIndex, element := range responseObject.Business {
+		for idx := 0; idx < int(element.Rating); idx++ {
+			if element.RatingDisplay == nil {
+				element.RatingDisplay = []bool{true}
+			} else {
+				element.RatingDisplay = append(element.RatingDisplay, true)
+			}
+
+		}
+		if element.Rating != math.Trunc(element.Rating) {
+			element.RatingHalf = true
+		}
+
+		responseObject.Business[elementIndex] = element
+	}
+
+	tmpl.Execute(responseWriter, responseObject)
 }
